29bbb86d4e46ed459b1a32dcb1d2ec01948c6f6c
dhrubojpaul <dhrubojpaul@gmail.com>
2019-07-17 09:47:19 +0600

    home

--- pages/home.js
+++ pages/home.js
@@ -7,9 +7,9 @@ var component = {
                     <span>Tus eu perfecto</span>
                 </template>
                 <v-card class="elevation-2">
-                    <v-card-title class="headline">Lorem ipsum</v-card-title>
+                    <v-card-title class="headline">Event</v-card-title>
                     <v-card-text>
-                        Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+                        Description
                     </v-card-text>
                 </v-card>
             </v-timeline-item>
