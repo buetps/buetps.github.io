117651775f6d7c9f1854e709ef492b4e0323a1be
dhrubojpaul <dhrubojpaul@gmail.com>
2020-05-21 17:31:20 -0400

    menu init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <v-app id="inspire">
+    
+    <v-navigation-drawer app floating>
+        <v-list rounded>
+          <v-list-item link v-for="(menuItem,menuIndex) in menu" :key=menuIndex :to="menuItem.link">
+            <v-list-item-content>
+              <v-list-item-title >{{menuItem.name}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
-    <v-content style="margin:0 10vw 10vh 10vw;">
+
+    <v-content>
       <v-container fluid>
         <transition name=slide-left mode=out-in>
           <router-view/>
@@ -12,6 +23,12 @@
 </template>
 
 <script>
+import menu from "./data/menu"
   export default {
+    data(){
+      return {
+        menu
+      }
+    }
   }
 </script>
---
+++ src/data/menu.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+        name: "Committee",
+        link: "/committee"
+    },
+    {
+        name: "People",
+        link: "/people"
+    },
+    {
+        name: "Voyage of Visuals",
+        link: "/exhibition/vov"
+    },
+    {
+        name: "Annual Exhibition",
+        link: "/exhibition/annual"
+    },
+    {
+        name: "Loop",
+        link: "/workshop/loop"
+    },
+    {
+        name: "Discovering Streets",
+        link: "/workshop/discoveringstreets"
+    },
+    {
+        name: "Photography Quintessentials",
+        link: "/workshop/quintessentials"
+    },
+];
+
+export default data;
