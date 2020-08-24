b0762c1782089d30b665659438db665a57bf41a8
dhrubojpaul <dhrubojpaul@gmail.com>
2020-08-24 18:52:53 -0400

    asif sami

---
+++ src/data/people/ahmad.asif.sami.js
@@ -0,0 +1,102 @@
+const data = 
+    {
+        id: "ahmad.asif.sami",
+        name: "Ahmad Asif Sami",
+        batch: "14",
+        links: {
+            instagram: ""
+        },
+        bio: [
+            "Ami Sami"
+        ],
+        albums: [
+            {
+                id: "life",
+                name: "Life",
+                description: [
+					"Each and Every being in this world has a beginning, as well as an ending.. Anything which served its purpose in this world may seem useless at first. But looking at another angle, one can find signs of life in “everything”. Life starts, life leaves its mark, life ends, and new life starts.",
+                    "Life, the most abused yet the most important thing in a human’s life."
+                ],
+                photos: [
+                    {
+                        id: "1",
+                    },
+                    {
+                        id: "2",
+                    },
+                    {
+                        id: "3",
+                    },
+                    {
+                        id: "4",
+                    },
+                ]
+            },
+            {
+                id: "reverence",
+                name: "Reverence",
+                description: [
+                    "Mecca!",
+
+                    "It’s the birthplace of Islam, a city of spirituality, a place where the holy Kaaba is situated. It’s a place to refine yourself, to purify your soul. It’s a city that never sleeps, that never rests, where prayer to the almighty never stops, where people present themselves as servants of their creator. Hundreds of thousands of people from all corners of the world with diverse walks of life chose to take a pilgrimage every year, leaving their familiar lands and loved ones for days.",
+
+                    "All of these, it’s for one ultimate reason. To be forgiven by the almighty, for a place in the paradise."
+
+                ],
+                photos: [
+                    {
+                        id: "Ahmad Asif Sami (1)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (2)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (3)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (4)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (5)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (6)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (7)",
+                    },
+                    {
+                        id: "Ahmad Asif Sami (8)",
+                    },
+					{
+                        id: "Ahmad Asif Sami (9)",
+                    },
+					{
+                        id: "Ahmad Asif Sami (10)",
+                    },
+                ]
+            },
+            {
+                id: "singles",
+                name: "Single",
+                description: [
+                    
+                ],
+                photos: [
+                    {
+                        id: "1",
+                    },
+                    {
+                        id: "2",
+                    },
+                    {
+                        id: "3",
+                    },
+                ]
+            },
+        ]
+
+    }
+;
+
+export default data;
--- src/data/people/index.js
+++ src/data/people/index.js
@@ -2,10 +2,14 @@ import AbdulAzizApu from "./abdul.aziz.apu"
 import NaserImranHossain from "./naser.imran.hossain"
 import FahimHossain from "./fahim.hossain"
 
+import AhmadAsifSami from "./ahmad.asif.sami"
+
 const data = [
     AbdulAzizApu,
     NaserImranHossain,
-    FahimHossain
+    FahimHossain,
+
+    AhmadAsifSami
 ];
 
 export default data;
